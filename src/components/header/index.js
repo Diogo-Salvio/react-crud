e0654b07f31bf51bec6fb6b1c96929eb64af0383
Diogo-Salvio <diogosalvio787@gmail.com>
2025-08-14 10:15:49 -0300

    Criação da função visual do Header

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>Crud Organizador de tarefas</h1>
+            <button>Criar nova tarefa</button>
+        </header>
+    )
+}
+
+export default Header
