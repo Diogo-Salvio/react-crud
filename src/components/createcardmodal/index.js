efad9d3bb19e758b252fc51767bfe15d6c8d48af
Diogo-Salvio <diogosalvio787@gmail.com>
2025-08-19 15:55:57 -0300

    Validação  que não permite adicionar uma data menor que a data do dia da criação da tarefa

--- src/components/createcardmodal/index.js
+++ src/components/createcardmodal/index.js
@@ -24,12 +24,17 @@ const CreateCardModal = ({
 
         const cardKeys = Object.values(card)
 
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const taskdate = new Date(cardKeys[1]);
+        taskdate.setHours(0, 0, 0, 0);
 
-        if (cardKeys[0].length === 0) {
+
+        if (!cardKeys[0].trim()) {
             alert('A tarefa precisa de um titulo!')
-        } else if (cardKeys[2].length === 0) {
+        } else if (!cardKeys[2].trim()) {
             alert('A tarefa precisa de uma descrição')
-        } else if (cardKeys[1].length !== 10) {
+        } else if (cardKeys[1].length !== 10 || today.getTime() > taskdate.getTime()) {
             alert('Adicione uma data valida')
         } else {
             const newCard = { ...card, key: Date.now() }
