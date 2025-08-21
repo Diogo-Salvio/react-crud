e2ae1c41f39aeafe34506618e371b459e75ee4d0
Diogo-Salvio <diogosalvio787@gmail.com>
2025-08-21 17:10:53 -0300

    funcionalidade do localstorage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import './App.css';
 import ContainerCards from './components/containercards';
 import Header from './components/header';
 
 
 function App() {
+  //Estados das tasks 
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
 
-  const [tasks, setTasks] = useState([
-    { title: "1", date: "2026-08-21", description: "1 da 1 de 1", key: 1001},
-    { title: "2", date: "2026-08-25", description: "2 da 2 de 2", key: 1002}
-  ])
-
-  const [finishTasks, setFinishTasks] = useState([{ title: "3", date: "2027-05-22", description: "3 da 3 de 3", key: 1003}])
-
+  const [finishTasks, setFinishTasks] = useState(() => {
+    const saved = localStorage.getItem("finishTasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+  //Estado para a criação do Card
   const [card, setCard] = useState({ title: "", date: "", description: "", key: 0 })
 
-  // useEffect(() => {
-  //   setTasks(prev =>  [...prev].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime() ))
-  //   setFinishTasks(prev => [...prev].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime() ))
-  //   // const date = new Date("2002-04-22")
-  //   // console.log(date.getTime())
-  // }, [])
-
-  const sortedTasks= useMemo(() => {
+  //Arrays das tarefas organizados
+  const sortedTasks = useMemo(() => {
     return [...tasks].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
   }, [tasks])
-
   const sortedFinishTasks = useMemo(() => {
     return [...finishTasks].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
   }, [finishTasks])
+
+  //Função para salvar os dados em localStorage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    localStorage.setItem("finishTasks", JSON.stringify(finishTasks))
+  }, [tasks, finishTasks])
+
+  // useEffect(() => {
+  //   const savedTasks = localStorage.getItem("tasks")
+  //   const savedFinishTasks = localStorage.getItem("finishTasks")
+
+  //   if(savedTasks) {
+  //     setTasks(JSON.parse(savedTasks))
+  //   }
+  //   if(savedFinishTasks) {
+  //     setFinishTasks(JSON.parse(savedFinishTasks))
+  //   }
+  // }, [])
 
   return (
     <div className='App'>
